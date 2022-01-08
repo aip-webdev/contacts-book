71f1483f6d129c4ea6b43c4e4878608e9287f286
petrenko_aleksei <aipgrandfa@yandex.ru>
2022-01-08 20:04:12 +0600

    Add layout for mobile

--- src/shared/Components/RequiredAuth.tsx
+++ src/shared/Components/RequiredAuth.tsx
@@ -1,15 +1,16 @@
-import {useNavigate} from "react-router-dom";
 import {useAppStore} from "../../hooks/useAppStore";
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
+import { Navigate } from "react-router-dom";
 
 export const RequiredAuth = ({ children }: { children: JSX.Element }) => {
     const [state] = useAppStore();
-    const navigate = useNavigate();
-    useEffect(() => {
-        if (!state.isAuth) {
-             navigate("/signin")
-        }
-    }, [])
+    const [rendered, setRendered] = useState(false)
 
+    useEffect(() => {
+        setRendered(true)
+    }, [])
+    if (rendered && !state.isAuth) {
+        return (<Navigate to='/signin'/>)
+    }
     return children
 }
